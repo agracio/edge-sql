fc0e6b669dca13b03c712bc5c1682e2a4f2addc8
agracio <antonas.graciovas@gmail.com>
2024-04-22 22:42:49 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,130 @@
-edge-sql
-=======
+## edge-sql
 
-This is a SQL compiler for edge.js. It allows accessing SQL databases from Node.js using Edge.js and ADO.NET. 
+### MS SQL Server compiler for [Edge.js](https://github.com/agracio/edge-js). 
 
-See [edge.js overview](http://tjanczuk.github.com/edge) and [edge.js on GitHub](https://github.com/tjanczuk/egde) for more information. 
+### This library is based on https://github.com/tjanczuk/edge-sql all credit for original work goes to Tomasz Janczuk. 
+-------
+
+## Overview
+* Supports returning multiple results from `select` queries
+* Supports any type of SQL statement allowing to run complex queries that declare variables, temp tables etc...
+
+**NOTE** SQL Server Geography and Geometry types are not supported.
+
+### SQL statement interpretation
+
+| SQL Statement   | C# Implemetation     |
+| --------------- | -------------------- |
+| select          | ExecuteReaderAsync   |
+| update          | ExecuteNonQueryAsync |
+| insert          | ExecuteNonQueryAsync |
+| delete          | ExecuteNonQueryAsync |
+| exec/execute    | ExecuteReaderAsync   |
+| other           | ExecuteReaderAsync   |
+
+### Parameters
+
+| Parameter          | Usage                |
+| ------------------ | -------------------- |
+| `connectionString` | Required. Use environment variable or input parameter |
+| `source`           | Optional if no other parameters are specified         |
+| `commandTimeout`   | Optional                                              |
+
+To better understand parameter usage see examples below.
+
+### Basic usage
+
+You can set your SQL connection string using environment variable. For passing connection string as a parameter see [Advanced usage](#advanced-usage).
+
+```
+set EDGE_SQL_CONNECTION_STRING=Data Source=localhost;Initial Catalog=Northwind;Integrated Security=True
+```
+
+#### Simple select
+
+```js
+const edge = require('edge-js');
+
+var getTop10Products = edge.func('sql', function () {/*
+    select top 10 * from Products
+*/});
+
+getTop10Products(null, function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+```
+
+#### Parameterized queries
+
+You can construct a parameterized query once and provide parameter values on a per-call basis:
+
+**SELECT**
+
+```js
+const edge = require('edge-js');
+
+var getProduct = edge.func('sql', function () {/*
+    select * from Products 
+    where ProductId = @myProductId
+*/});
+
+getProduct({ myProductId: 10 }, function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+```
+
+**UPDATE**
+
+```js
+const edge = require('edge-js');
+
+var updateProductName = edge.func('sql', function () {/*
+    update Products
+    set ProductName = @newName 
+    where ProductId = @myProductId
+*/});
+
+updateProductName({ myProductId: 10, newName: 'New Product' }, function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+```
+
+### Advanced usage
+ 
+##### Using parameterized function
+
+```js
+const edge = require('edge-js');
+
+var select = edge.func('sql', {
+    source: 'select top 10 * from Products',
+    connectionString: 'SERVER=myserver;DATABASE=mydatabase;Integrated Security=SSPI',
+    commandTimeout: 100
+});
+
+select(null, function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+```
+ 
+##### Stored proc with input parameters  
+
+ ```js
+const edge = require('edge-js');
+
+var storedProcParams = {inputParm1: 'input1', inputParam2: 25};
+
+var select = edge.func('sql', {
+    source: 'exec myStoredProc',
+    connectionString: 'SERVER=myserver;DATABASE=mydatabase;Integrated Security=SSPI'
+});
+
+select(storedProcParams, function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+```  
---
+++ src/compiler/EdgeCompiler.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+public class EdgeCompiler
+{
+    public Func<object, Task<object>> CompileFunc(IDictionary<string, object> parameters)
+    {
+        string command = ((string)parameters["source"]).TrimStart();
+        string connectionString = Environment.GetEnvironmentVariable("EDGE_SQL_CONNECTION_STRING");
+        int? commandTimeout = null;
+
+        if (parameters.TryGetValue("connectionString", out var connectionStringTmp))
+        {
+            connectionString = (string)connectionStringTmp;
+        }
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentException("connectionString must be specified using EDGE_SQL_CONNECTION_STRING environmental variable or passed as parameter.");
+        }
+
+        if (parameters.TryGetValue("commandTimeout", out var commandTimeoutTmp))
+        {
+            commandTimeout = (int)commandTimeoutTmp;
+        }
+
+        if (command.StartsWith("select ", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return async (queryParameters) => await 
+            ExecuteQuery(
+                connectionString, 
+                command, 
+                (IDictionary<string, object>)queryParameters, 
+                commandTimeout);
+        }
+        if (command.StartsWith("insert ", StringComparison.InvariantCultureIgnoreCase)
+            || command.StartsWith("update ", StringComparison.InvariantCultureIgnoreCase)
+            || command.StartsWith("delete ", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return async (queryParameters) => await 
+            ExecuteNonQuery(
+                connectionString, 
+                command, (IDictionary<string, object>)queryParameters, 
+                commandTimeout);
+        }
+        if (command.StartsWith("exec ", StringComparison.InvariantCultureIgnoreCase) || command.StartsWith("execute ", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return async (queryParameters) => await
+                ExecuteStoredProcedure(
+                    connectionString,
+                    command,
+                    (IDictionary<string, object>)queryParameters,
+                    commandTimeout);
+        }
+        // Use ExecuteQuery for any other SQL commands 
+        return async (queryParameters) => await 
+            ExecuteQuery(
+                connectionString, 
+                command, 
+                (IDictionary<string, object>)queryParameters, 
+                commandTimeout);
+    }
+
+    void AddParameters(SqlCommand command, IDictionary<string, object> parameters)
+    {
+        if (parameters != null)
+        {
+            foreach (var parameter in parameters)
+            {
+                if (parameter.Key == "returnParameter")
+                {
+                    String returnParameterName = "@" + parameter.Value.ToString();
+                    command.Parameters.Add(returnParameterName, SqlDbType.NVarChar, -1);
+                    command.Parameters[returnParameterName].Direction = ParameterDirection.Output;
+                }
+                else
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value ?? DBNull.Value);
+                }
+            }
+        }
+    }
+
+    async Task<object> ExecuteQuery(string connectionString, string commandString, IDictionary<string, object> parameters, int? commandTimeout = null)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (var command = new SqlCommand(commandString, connection))
+            {
+                if (commandTimeout.HasValue) command.CommandTimeout = commandTimeout.Value;
+                return await ExecuteQuery(parameters, command, connection);
+            }
+        }
+    }
+
+    async Task<object> ExecuteQuery(IDictionary<string, object> parameters, SqlCommand command, SqlConnection connection)
+    {
+        AddParameters(command, parameters);
+        var results = new Dictionary<string, object>();
+        await connection.OpenAsync();
+        var resultCount = new Dictionary<string, int>();
+        using (SqlDataReader reader = await command.ExecuteReaderAsync(CommandBehavior.KeyInfo))
+        {
+            do
+            {
+                var tableRows = reader.GetSchemaTable()?.Rows;
+                string table = string.Empty;
+                if (tableRows?.Count != 0)
+                {
+                    table = tableRows?[0]["BaseTableName"]?.ToString();
+                }
+
+                var resultName = string.IsNullOrEmpty(table) ? "result" : table;
+                if (!resultCount.ContainsKey(resultName))
+                {
+                    resultCount.Add(resultName, 0);
+                }
+
+                if (results.ContainsKey(resultName))
+                {
+                    resultCount[resultName]++;
+                }
+                
+                if (results.ContainsKey(resultName))
+                {
+                    resultName = $"{resultName}-{resultCount[resultName]}";
+                }
+                
+                var rows = new List<object>();
+                IDataRecord record = reader;
+                while (await reader.ReadAsync())
+                {
+                    var dataObject = new ExpandoObject() as IDictionary<string, object>;
+                    var resultRecord = new object[record.FieldCount];
+                    record.GetValues(resultRecord);
+
+                    for (int i = 0; i < record.FieldCount; i++)
+                    {      
+                        Type type = record.GetFieldType(i);
+                        if (resultRecord[i] is DBNull)
+                        {
+                            resultRecord[i] = null;
+                        }
+                        else if (type == typeof(short) || type == typeof(ushort)) {
+                            resultRecord[i] = Convert.ToInt32(resultRecord[i]);
+                        }
+                        else if (type == typeof(Decimal)) {
+                            resultRecord[i] = Convert.ToDouble(resultRecord[i]);
+                        }
+                        else if (type == typeof(byte[]) || type == typeof(char[]))
+                        {
+                            resultRecord[i] = Convert.ToBase64String((byte[])resultRecord[i]);
+                        }
+                        else if (type == typeof(Guid) || type == typeof(DateTime))
+                        {
+                            resultRecord[i] = resultRecord[i].ToString();
+                        }
+                        else if (type == typeof(IDataReader))
+                        {
+                            resultRecord[i] = "<IDataReader>";
+                        }
+
+                        dataObject.Add(record.GetName(i), resultRecord[i]);
+                    }
+
+                    rows.Add(dataObject);
+                }
+                results.Add(resultName, rows);
+            } while (await reader.NextResultAsync());
+
+            return results.Keys.Count == 1 ? results[results.Keys.First()] : results;
+        }
+    }
+
+    async Task<object> ExecuteNonQuery(string connectionString, string commandString, IDictionary<string, object> parameters, int? commandTimeout = null)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            using (var command = new SqlCommand(commandString, connection))
+            {
+                if (commandTimeout.HasValue) command.CommandTimeout = commandTimeout.Value;
+                AddParameters(command, parameters);
+                await connection.OpenAsync();
+                return await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+
+    async Task<object> ExecuteStoredProcedure(string connectionString, string commandString, IDictionary<string, object> parameters, int? commandTimeout = null)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            var trim = commandString.StartsWith("execute ", StringComparison.InvariantCultureIgnoreCase) ? 8 : 5;
+            
+            SqlCommand command = new SqlCommand(commandString.Substring(trim).TrimEnd(), connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            if (commandTimeout.HasValue) command.CommandTimeout = commandTimeout.Value;
+            using (command)
+            {
+                return await ExecuteQuery(parameters, command, connection);
+            }
+        }
+    }
+}
