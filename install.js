295486f5de7883af88f30d51d48f55ce3677cc6b
agracio <agracio@users.noreply.github.com>
2025-05-16 00:26:56 +0100

    updating binaries and install script

--- install.js
+++ install.js
@@ -1,33 +1,8 @@
 const path = require('path')
 const fs = require('fs');
 
-if (process.platform === 'win32'){
-    if(process.arch === 'x64'){
-        copy('win-x64', true);
-    }
-    else if(process.arch === 'arm64'){
-        copy('win-arm64', true);
-    }
-    else if(process.arch === 'ia32'){
-        copy('win-x86', true);
-    }
-    else{
-        archError();
-    }
-}
-else if(process.platform === 'darwin'){
-    copy('darwin');
-}
-else if(process.platform === 'linux'){
-    copy('linux');
-}
-else{
-    throw new Error(`Platform '${process.platform}' is not supported. `);
-}
-
-function archError(){
-    throw new Error(`Process architecture '${process.arch}' is not supported on '${process.platform}'`);
-}
+let sni = process.platform === 'win32';
+copy(process.platform, sni);
 
 function copy(source, sni) {
 
